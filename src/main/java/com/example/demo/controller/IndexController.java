588530bc657572b2c8f75104bee293a41d691116
RyunK, Lee <lrk222@naver.com>
2026-09-22 11:14:21 +0900

    index controller

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+
+@Controller 
+public class IndexController {
+    @GetMapping("/")
+    public @ResponseBody String index() {
+        return "Hello, world!";
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "views/hello";
+    }
+    
+    
+}
